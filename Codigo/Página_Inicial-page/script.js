abb002010696706b01a20a0246bea7dadf4a1704
Rubia Mattos <rubia.matos@hotmail.com>
2021-06-29 18:08:29 -0300

    Biblioteca JS

--- Codigo/Página_Inicial-page/script.js
+++
@@ -1,4 +0,0 @@
-let texto = 'Breathe - Todos os direitos reservados - 2021';
-let imagem = 'img/logo2.png';
-let txtHTML = `<div class="row"><div class="col-9 col-lg-11 footer_area">${texto}</div><div class="col-3 col-lg-1"><img src="${imagem}"></div></div>`;
-document.querySelector('.footer').innerHTML = txtHTML;
